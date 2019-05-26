c470f34c3f764ad0c4ea4612117ab75bd0fa30b9
Mate Soos <soos.mate@gmail.com>
2019-05-26 22:56:27 +0200

    Fixing copyright

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -1,5 +1,6 @@
 /******************************************
 Copyright (c) 2019 Jo Devriendt - KU Leuven
+Copyright (c) 2019 Bart Bogaerts
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
