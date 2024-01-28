2c4de3b368ae8c1fcc8be0bb273f496977f543f5
Mate Soos <soos.mate@gmail.com>
2024-01-28 21:49:03 +0100

    Making it build under Windows

--- src/breakid.hpp
+++ src/breakid.hpp
@@ -20,8 +20,14 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ***********************************************/
 
-#ifndef BREAKID_H
-#define BREAKID_H
+#pragma once
+
+#if defined _WIN32
+    #define DLL_PUBLIC __declspec(dllexport)
+#else
+    #define DLL_PUBLIC __attribute__ ((visibility ("default")))
+    #define DLL_LOCAL  __attribute__ ((visibility ("hidden")))
+#endif
 
 #include <string>
 #include <vector>
@@ -36,62 +42,36 @@ struct PrivateData;
 class BLit
 {
     uint32_t x;
-    explicit BLit(uint32_t i) : x(i) { }
+    constexpr explicit BLit(uint32_t i) : x(i) { }
 public:
-    BLit() : x((0xffffffffU >> 3)<<1) {}   // (lit_Undef)
-    explicit BLit(uint32_t var, bool is_inverted) :
+    constexpr BLit() : x((0xffffffffU >> 3)<<1) {}   // (lit_Undef)
+    constexpr explicit BLit(uint32_t var, bool is_inverted) :
         x((var<<1) | (uint32_t)is_inverted)
     {}
 
-    const uint32_t& toInt() const { // Guarantees small, positive integers suitable for array indexing.
-        return x;
-    }
-    BLit  operator~() const {
-        return BLit(x ^ 1);
-    }
-    BLit  operator^(const bool b) const {
-        return BLit(x ^ (uint32_t)b);
-    }
-    BLit& operator^=(const bool b) {
+    constexpr const uint32_t& toInt() const { return x; }
+    constexpr BLit  operator~() const { return BLit(x ^ 1); }
+    constexpr BLit  operator^(const bool b) const { return BLit(x ^ (uint32_t)b); }
+    constexpr BLit& operator^=(const bool b) {
         x ^= (uint32_t)b;
         return *this;
     }
-    bool sign() const {
-        return x & 1;
-    }
-    uint32_t  var() const {
-        return x >> 1;
-    }
-    BLit  unsign() const {
-        return BLit(x & ~1U);
-    }
-    bool operator==(const BLit& p) const {
-        return x == p.x;
-    }
-    bool operator!= (const BLit& p) const {
-        return x != p.x;
-    }
-    /**
-    @brief ONLY to be used for ordering such as: a, b, ~b, etc.
-    */
-    bool operator <  (const BLit& p) const {
-        return x < p.x;     // '<' guarantees that p, ~p are adjacent in the ordering.
-    }
-    bool operator >  (const BLit& p) const {
-        return x > p.x;
-    }
-    bool operator >=  (const BLit& p) const {
-        return x >= p.x;
-    }
-    static BLit toBLit(uint32_t data)
-    {
-        return BLit(data);
+    constexpr bool sign() const { return x & 1; }
+    constexpr uint32_t  var() const { return x >> 1; }
+    constexpr BLit  unsign() const { return BLit(x & ~1U); }
+    constexpr bool operator==(const BLit& p) const { return x == p.x; }
+    constexpr bool operator!= (const BLit& p) const { return x != p.x; }
+
+    constexpr bool operator <  (const BLit& p) const { return x < p.x; }
+    constexpr bool operator >  (const BLit& p) const { return x > p.x; }
+    constexpr bool operator >=  (const BLit& p) const { return x >= p.x; }
+    constexpr static BLit toBLit(uint32_t data) { return BLit(data);
     }
 };
 
-static const BLit BLit_Undef = BLit::toBLit((0xffffffffU >> 3)<<1);
+constexpr BLit BLit_Undef = BLit::toBLit((0xffffffffU >> 3)<<1);
 
-inline std::ostream& operator<<(std::ostream& os, const BLit lit)
+DLL_PUBLIC inline std::ostream& operator<<(std::ostream& os, const BLit lit)
 {
 
     os << (lit.sign() ? "-" : "") << (lit.var() + 1);
@@ -99,43 +79,43 @@ inline std::ostream& operator<<(std::ostream& os, const BLit lit)
 }
 
 struct BreakID {
-    BreakID();
-    ~BreakID();
+    DLL_PUBLIC BreakID();
+    DLL_PUBLIC ~BreakID();
 
-    std::string get_sha1_version() const;
+    DLL_PUBLIC std::string get_sha1_version() const;
 
     //configuration functions
-    void set_useMatrixDetection(bool val);
-    void set_useBinaryClauses(bool val);
-    void set_useShatterTranslation(bool val);
-    void set_useFullTranslation(bool val);
-    void set_symBreakingFormLength(int val);
-    void set_verbosity(uint32_t val);
-    void set_steps_lim(int64_t val);
+    DLL_PUBLIC void set_useMatrixDetection(bool val);
+    DLL_PUBLIC void set_useBinaryClauses(bool val);
+    DLL_PUBLIC void set_useShatterTranslation(bool val);
+    DLL_PUBLIC void set_useFullTranslation(bool val);
+    DLL_PUBLIC void set_symBreakingFormLength(int val);
+    DLL_PUBLIC void set_verbosity(uint32_t val);
+    DLL_PUBLIC void set_steps_lim(int64_t val);
 
     //Dynamic CNF
-    void start_dynamic_cnf(uint32_t nVars);
-    void add_clause(BID::BLit* start, size_t num);
-    void end_dynamic_cnf();
+    DLL_PUBLIC void start_dynamic_cnf(uint32_t nVars);
+    DLL_PUBLIC void add_clause(BID::BLit* start, size_t num);
+    DLL_PUBLIC void end_dynamic_cnf();
 
     //Main functions
-    void detect_subgroups();
-    void break_symm();
+    DLL_PUBLIC void detect_subgroups();
+    DLL_PUBLIC void break_symm();
 
     //Print info
-    void print_subgroups(std::ostream& out);
-    void print_symm_break_stats();
-    void print_perms_and_matrices(std::ostream& out);
-    void print_generators(std::ostream& out);
+    DLL_PUBLIC void print_subgroups(std::ostream& out);
+    DLL_PUBLIC void print_symm_break_stats();
+    DLL_PUBLIC void print_perms_and_matrices(std::ostream& out);
+    DLL_PUBLIC void print_generators(std::ostream& out);
 
     //Get info
-    uint32_t get_num_generators();
-    uint32_t get_num_break_cls();
-    uint32_t get_num_aux_vars();
-    int64_t get_steps_remain() const;
-    uint64_t get_num_subgroups() const;
-    std::vector<std::vector<BID::BLit>> get_brk_cls();
-    void get_perms(std::vector<std::unordered_map<BLit, BLit> >* out);
+    DLL_PUBLIC uint32_t get_num_generators();
+    DLL_PUBLIC uint32_t get_num_break_cls();
+    DLL_PUBLIC uint32_t get_num_aux_vars();
+    DLL_PUBLIC int64_t get_steps_remain() const;
+    DLL_PUBLIC uint64_t get_num_subgroups() const;
+    DLL_PUBLIC std::vector<std::vector<BID::BLit>> get_brk_cls();
+    DLL_PUBLIC void get_perms(std::vector<std::unordered_map<BLit, BLit> >* out);
 
 private:
     BID::PrivateData* dat = NULL;
@@ -144,15 +124,9 @@ private:
 }
 
 namespace std {
-
-  template <>
-  struct hash<BID::BLit>
-  {
-    std::size_t operator()(const BID::BLit& k) const
-    {
+  template <> struct hash<BID::BLit> {
+    std::size_t operator()(const BID::BLit& k) const {
       return k.toInt();
     }
   };
-
 }
-#endif
