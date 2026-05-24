37a72c5296a6d3d04fd965bb91a2234653dc0e28
Mate Soos <soos.mate@gmail.com>
2026-05-24 10:32:44 +0200

    Add notest

---
+++ scripts/build_notest.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+rm -rf Make* cm* CM* lib* bliss breakid include src lib breakidConfig.cmake breakidTargets.cmake
+
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DENABLE_TESTING=OFF ..
+make -j$(nproc)
